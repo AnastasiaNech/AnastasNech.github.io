a769c1bdf1d45576aa5e0d99fb6fd00efbf0dae1
AnastasiaNech <nechaevaanastasiamih@gmail.com>
2023-01-28 23:27:37 +0300

    fix all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { Telegraf } from 'telegraf';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,14 +6,6 @@ import { Telegraf } from 'telegraf';
 })
 export class AppComponent{
   
-  public bot: Telegraf = new Telegraf('5836658333:AAGM2lRF_sDjsxliWs263MW9PElVQj3-cBs');
-
-  constructor(){
-    this.bot.start((ctx) => {
-       ctx.reply('Hello ' + ctx.from.first_name + '!');
-     });
-    this.bot.launch();
-  }
 
   title = 'myStore';
 }
