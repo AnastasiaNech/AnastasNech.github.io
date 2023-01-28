267a5a578cc7446cc74b273c8150198cc1216f16
AnastasiaNech <nechaevaanastasiamih@gmail.com>
2023-01-28 18:09:02 +0300

    change start

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -8,8 +8,10 @@ import { Component,OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   
   constructor(){
+   
   }
   ngOnInit(): void {
+
     this.initButton();
   }
   public test1: string = "";
